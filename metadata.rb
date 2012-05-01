e12994d743155070b4abe36ffceeb1b5a9983fb9
Cyril Mougel <cyril.mougel@gmail.com>
2012-05-01 17:09:19 +0200

    add a metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'mongodb'
+version '1.0.0'
+description 'A mongoDB recipe'
+maintainer 'Cyril Mougel'
+maintainer_email 'cyril.mougel@gmail.com'
+license 'MIT'
+provides "mongodb::default"
+supports 'ubuntu'
